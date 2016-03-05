9de720b84203b5f71092dc0925bb1a8c51a72dc4
softcadbury <romain.parage@gmail.com>
2016-03-05 13:56:54 +0100

    Code cleaning

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
         widget_base_dimensions: [100, 100]
     }).data('gridster');
 
-    Add('/tables/premier-league', 4, 4);
+    add('/tables/premier-league', 4, 4);
 
-    Add('/tables/mini/premier-league', 2, 2);
-    Add('/assists/mini/premier-league', 2, 2);
-    Add('/scorers/mini/premier-league', 2, 2);
+    add('/tables/mini/premier-league', 2, 2);
+    add('/assists/mini/premier-league', 2, 2);
+    add('/scorers/mini/premier-league', 2, 2);
 
-    Add('/scorers/premier-league', 3, 4);
-    Add('/scorers/ligue-1', 3, 4);
-    Add('/scorers/bundesliga', 3, 4);
+    add('/scorers/premier-league', 3, 4);
+    add('/scorers/ligue-1', 3, 4);
+    add('/scorers/bundesliga', 3, 4);
 
-    Add('/assists/premier-league', 3, 4);
-    Add('/assists/ligue-1', 3, 4);
-    Add('/assists/bundesliga', 3, 4);
+    add('/assists/premier-league', 3, 4);
+    add('/assists/ligue-1', 3, 4);
+    add('/assists/bundesliga', 3, 4);
 
-    function Add(url, sizeX, sizeY) {
+    function add(url, sizeX, sizeY) {
         $.ajax({
             type: 'GET',
             url: url,
