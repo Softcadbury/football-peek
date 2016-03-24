3e87b83284d7f796b397d4cb936bb334e19e1970
Romain Parage <rparage@talentsoft.com>
2016-03-24 14:19:19 +0100

    Add tables dynamically

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,37 +4,4 @@ var GridConfigurationViewModel = require('./viewmodels/gridConfiguration.viewmod
 
 $(document).ready(function() {
     ko.applyBindings(new GridConfigurationViewModel(), document.getElementById('test'));
-
-    var gridster = $('.gridster').gridster({
-        widget_margins: [5, 5],
-        widget_base_dimensions: [100, 100]
-    }).data('gridster');
-
-    add('/tables/bundesliga', 4, 4);
-    add('/tables/liga', 4, 4);
-    add('/tables/ligue-1', 4, 4);
-    add('/tables/premier-league', 4, 4);
-    add('/tables/serie-a', 4, 4);
-
-    add('/tables/mini/premier-league', 2, 2);
-    add('/assists/mini/premier-league', 2, 2);
-    add('/scorers/mini/premier-league', 2, 2);
-
-    add('/scorers/premier-league', 3, 4);
-    add('/scorers/ligue-1', 3, 4);
-    add('/scorers/bundesliga', 3, 4);
-
-    add('/assists/premier-league', 3, 4);
-    add('/assists/ligue-1', 3, 4);
-    add('/assists/bundesliga', 3, 4);
-
-    function add(url, sizeX, sizeY) {
-        $.ajax({
-            type: 'GET',
-            url: url,
-            success: function(data) {
-                gridster.add_widget.apply(gridster, [data, sizeX, sizeY]);
-            }
-        });
-    }
 });
--- client/scripts/viewmodels/gridConfiguration.viewmodel.js
+++ client/scripts/viewmodels/gridConfiguration.viewmodel.js
@@ -5,8 +5,13 @@ var leagues = require('../../../common/leagues');
 function GridConfigurationViewModel() {
     var components = ko.observableArray();
 
+    var gridster = $('.gridster').gridster({
+        widget_margins: [5, 5],
+        widget_base_dimensions: [100, 100]
+    }).data('gridster');
+
     for (var item in leagues) {
-        components.push({
+        var component = {
             league: leagues[item],
             displayTable: ko.observable(false),
             displayTableMini: ko.observable(false),
@@ -14,6 +19,33 @@ function GridConfigurationViewModel() {
             displayScorersMini: ko.observable(false),
             displayAssists: ko.observable(false),
             displayAssistsMini: ko.observable(false)
+        };
+
+        (function(league) {
+            component.displayTable.subscribe(function(newValue) {
+                if (newValue) {
+                    add('/tables/' + league.code, 4, 4);
+                    add('/tables/mini/' + league.code, 2, 2);
+                    add('/scorers/' + league.code, 3, 4);
+                    add('/scorers/mini/' + league.code, 2, 2);
+                    add('/assists/' + league.code, 3, 4);
+                    add('/assists/mini/' + league.code, 2, 2);
+                } else {
+                    // todo remove component
+                }
+            });
+        })(leagues[item]);
+
+        components.push(component);
+    }
+
+    function add(url, sizeX, sizeY) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(data) {
+                gridster.add_widget.apply(gridster, [data, sizeX, sizeY]);
+            }
         });
     }
 
