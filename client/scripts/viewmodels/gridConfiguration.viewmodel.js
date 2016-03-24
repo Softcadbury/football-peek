9c1e9986a1f2a3e44071b5dba7309f0ba8aa15ea
Romain Parage <rparage@talentsoft.com>
2016-03-24 18:00:45 +0100

    Allow to add different components

--- client/scripts/viewmodels/gridConfiguration.viewmodel.js
+++ client/scripts/viewmodels/gridConfiguration.viewmodel.js
@@ -22,23 +22,27 @@ function GridConfigurationViewModel() {
         };
 
         (function(league) {
-            component.displayTable.subscribe(function(newValue) {
-                if (newValue) {
-                    add('/tables/' + league.code, 4, 4);
-                    add('/tables/mini/' + league.code, 2, 2);
-                    add('/scorers/' + league.code, 3, 4);
-                    add('/scorers/mini/' + league.code, 2, 2);
-                    add('/assists/' + league.code, 3, 4);
-                    add('/assists/mini/' + league.code, 2, 2);
-                } else {
-                    // todo remove component
-                }
-            });
+            component.displayTable.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/tables/' + league.code, 4, 4); });
+            component.displayTableMini.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/tables/mini/' + league.code, 2, 2); });
+            component.displayScorers.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/scorers/' + league.code, 3, 4); });
+            component.displayScorersMini.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/scorers/mini/' + league.code, 2, 2); });
+            component.displayAssists.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/assists/' + league.code, 3, 4); });
+            component.displayAssistsMini.subscribe(function(newValue) { manageNewDisplayValue(newValue, '/assists/mini/' + league.code, 2, 2); });
         })(leagues[item]);
 
         components.push(component);
     }
 
+    // Manage the new value of the display
+    function manageNewDisplayValue(newValue, url, sizeX, sizeY) {
+        if (newValue) {
+            add(url, sizeX, sizeY);
+        } else {
+            // todo remove component
+        }
+    }
+
+    // Add a component in the grid
     function add(url, sizeX, sizeY) {
         $.ajax({
             type: 'GET',
