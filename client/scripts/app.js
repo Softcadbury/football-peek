e29e7994261e03dcda34f456015129838411bb28
Romain Parage <rparage@talentsoft.com>
2016-03-24 17:52:01 +0100

    Code cleaning

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,5 +3,5 @@
 var GridConfigurationViewModel = require('./viewmodels/gridConfiguration.viewmodel');
 
 $(document).ready(function() {
-    ko.applyBindings(new GridConfigurationViewModel(), document.getElementById('test'));
+    ko.applyBindings(new GridConfigurationViewModel(), document.getElementById('grid-configuration'));
 });
