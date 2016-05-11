a60e3b7b3eac1a3936b3117ff32aa0c2d75ef18b
Romain Parage <rparage@talentsoft.com>
2016-05-11 17:07:38 +0200

    Try to make it work on Azure

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 'use strict';
 
-var config = require('./server/config');
-var express = require('express');
-var handlebars = require('express-handlebars');
-var app = express();
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
 
-// Middlewares configuration
-app.use(express.static('build'));
-app.use(express.static('data', { 'extensions': ['png', 'js'] }));
+// var config = require('./server/config');
+// var express = require('express');
+// var handlebars = require('express-handlebars');
+// var app = express();
 
-// Handlebars configuration
-app.set('views', 'client/views');
-app.engine('.hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+// // Middlewares configuration
+// app.use(express.static('build'));
+// app.use(express.static('data', { 'extensions': ['png', 'js'] }));
 
-// Starts application listening
-app.listen(config.port, (err) => {
-    console.log('running on ' + config.port);
-});
+// // Handlebars configuration
+// app.set('views', 'client/views');
+// app.engine('.hbs', handlebars({ extname: '.hbs' }));
+// app.set('view engine', '.hbs');
 
-// Starts crons
-require('./server/crons/updaterCron').start();
+// // Starts application listening
+// app.listen(config.port, (err) => {
+//     console.log('running on ' + config.port);
+// });
 
-// Regsiters routes
-var indexRoutes = require('./server/routes/indexRoutes');
-app.use('/', indexRoutes);
+// // Starts crons
+// require('./server/crons/updaterCron').start();
 
-var componentsRoutes = require('./server/routes/componentsRoutes');
-app.use('/components', componentsRoutes);
+// // Regsiters routes
+// var indexRoutes = require('./server/routes/indexRoutes');
+// app.use('/', indexRoutes);
+
+// var componentsRoutes = require('./server/routes/componentsRoutes');
+// app.use('/components', componentsRoutes);
