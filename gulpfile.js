426744357ffda3ca91d504f50ed176b372b4bd86
Romain Parage <rparage@talentsoft.com>
2016-05-11 17:23:07 +0200

    Revert tries

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// Updates data
+gulp.task('update', () => {
+    require('./server/updaters/tablesUpdater').update();
+    require('./server/updaters/scorersUpdater').update();
+    require('./server/updaters/assistsUpdater').update();
+    require('./server/updaters/resultsUpdater').update();
+});
+
+// Updates logos
+gulp.task('update-logos', () => {
+    require('./server/updaters/logosUpdater').update();
+});
+
+// Check coding rules
+gulp.task('check', () => {
+    var jshint = require('gulp-jshint');
+    var jscs = require('gulp-jscs');
+    gulp.src(['./*.js', './common/**/*.js', './server/**/*.js', './client/scripts/**/*.js'])
+        .pipe(jscs())
+        .pipe(jscs.reporter())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});
+
+// Build the application
+gulp.task('build', () => {
+    var browserify = require('gulp-browserify');
+    var uglify = require('gulp-uglify');
+    gulp.src('client/scripts/app.js')
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
+
+    var less = require('gulp-less');
+    var minifyCSS = require('gulp-minify-css');
+    var concatCss = require('gulp-concat-css');
+    gulp.src('./client/styles/**/*.less')
+        .pipe(less())
+        .pipe(concatCss("app.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/css'));
+});
+
+// Start the node server
+gulp.task('start', () => {
+    var nodemon = require('gulp-nodemon');
+    var options = {
+        script: 'server.js',
+        delayTime: 1,
+        env: { 'PORT': 5000 },
+        watch: ['server']
+    };
+
+    return nodemon(options).on('restart', () => {
+        console.log('Restarting...');
+    });
+});
+
+// Manage build, start the node server and open the browser
+gulp.task('default', ['build', 'start'], () => {
+    gulp.watch(['./common/**/*.js', './client/scripts/**/*', './client/styles/**/*'], ['inject']);
+
+    var openBrowser = require('gulp-open');
+    gulp.src('/').pipe(openBrowser({ uri: '127.0.0.1:5000', app: 'chrome' }));
+});
--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 'use strict';
 
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var config = require('./server/config');
+var express = require('express');
+var handlebars = require('express-handlebars');
+var app = express();
 
-// var config = require('./server/config');
-// var express = require('express');
-// var handlebars = require('express-handlebars');
-// var app = express();
+// Middlewares configuration
+app.use(express.static('build'));
+app.use(express.static('data', { 'extensions': ['png', 'js'] }));
 
-// // Middlewares configuration
-// app.use(express.static('build'));
-// app.use(express.static('data', { 'extensions': ['png', 'js'] }));
+// Handlebars configuration
+app.set('views', 'client/views');
+app.engine('.hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
-// // Handlebars configuration
-// app.set('views', 'client/views');
-// app.engine('.hbs', handlebars({ extname: '.hbs' }));
-// app.set('view engine', '.hbs');
+// Starts application listening
+app.listen(config.port, (err) => {
+    console.log('running on ' + config.port);
+});
 
-// // Starts application listening
-// app.listen(config.port, (err) => {
-//     console.log('running on ' + config.port);
-// });
+// Starts crons
+require('./server/crons/updaterCron').start();
 
-// // Starts crons
-// require('./server/crons/updaterCron').start();
+// Regsiters routes
+var indexRoutes = require('./server/routes/indexRoutes');
+app.use('/', indexRoutes);
 
-// // Regsiters routes
-// var indexRoutes = require('./server/routes/indexRoutes');
-// app.use('/', indexRoutes);
-
-// var componentsRoutes = require('./server/routes/componentsRoutes');
-// app.use('/components', componentsRoutes);
+var componentsRoutes = require('./server/routes/componentsRoutes');
+app.use('/components', componentsRoutes);
