e7a60577d2ba76a97514d521dbd0ddf0caa31a2a
softcadbury <romain.parage@gmail.com>
2016-06-02 22:41:55 +0200

    Add sitemap

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -3,24 +3,39 @@
 var config = require('../config');
 var helper = require('../helper');
 var leagues = require('../leagues');
+var sm = require('sitemap');
 var express = require('express');
 var router = express.Router();
+
+// Route for sitemap
+var sitemap = sm.createSitemap({ hostname: 'http://dashboardfootball.com', cacheTime: 600000 });
+sitemap.add({ url: '', changefreq: 'daily' });
+
+for (var item in leagues) {
+    sitemap.add({ url: '/' + leagues[item].code, changefreq: 'daily' });
+}
+
+router.route('/sitemap.xml')
+    .get((req, res) => {
+        res.header('Content-Type', 'application/xml');
+        res.send(sitemap.toString());
+    });
 
 // Route for index
 router.route('/:league?')
     .get((req, res) => {
         var currentLeague = null;
-        
+
         for (var item in leagues) {
             leagues[item].isActive = false;
             if (req.params.league == leagues[item].code) {
                 currentLeague = leagues[item];
             }
         }
-        
+
         currentLeague = currentLeague || leagues.bundesliga;
         currentLeague.isActive = true;
-        
+
         var data = {
             leagues: leagues,
             resultsData: helper.readJsonFile(helper.stringFormat(config.paths.resultsData, currentLeague.code)),
