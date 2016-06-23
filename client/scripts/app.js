dc8763983c45e567f4ec846b2dbe9003fdd10c35
Romain Parage <rparage@talentsoft.com>
2016-06-23 09:55:15 +0200

    Complete year dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hoho')
+$(document).ready(function () {
+    $(document).on('mouseup', function (e) {
+        if ($(e.target).is('#dropdown-label-years') || $(e.target).is('#dropdown-arrow-years')) {
+            $("#dropdown-content-years").toggle();
+        } else {
+            $("#dropdown-content-years").hide();
+        }        
+    });
+});
