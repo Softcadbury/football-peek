124a3681c2af4c09f78f4ace01676f4a61126e5b
Romain Parage <rparage@talentsoft.com>
2016-07-01 14:13:17 +0200

    Add base for tests

---
+++ tests/testData.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var assert = require('chai').assert;
+var config = require('../server/config');
+var helper = require('../server/helper');
+var leagues = require('../server/data/leagues');
+var competitions = require('../server/data/competitions');
+
+describe('Data intergrity', function () {
+    describe('leagues', function () {
+        describe('table', function () {
+            it('should have the right number of teams', function () {
+            });
+        });
+    });
+
+    describe('competitions', function () {
+    });
+});
