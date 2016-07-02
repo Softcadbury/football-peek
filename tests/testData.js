1372ce7b91e8fa9debbecf713bcc6bd4add8c332
softcadbury <romain.parage@gmail.com>
2016-07-02 23:21:33 +0200

    Add test to avoid empty data

--- tests/testData.js
+++ tests/testData.js
@@ -27,26 +27,42 @@ describe('Data intergrity', () => {
 });
 
 function testTableData(code, year) {
-    var tableData = helper.readJsonFile(helper.stringFormat(config.paths.tableData, code, year));
+    var data = helper.readJsonFile(helper.stringFormat(config.paths.tableData, code, year));
 
     it('Table should have the right number of teams', () => {
         var expectedNumber = code == leagues.bundesliga.code ? 18 : 20;
-        assert.equal(expectedNumber, tableData.length);
+        assert.lengthOf(data, expectedNumber);
     });
+
+    testDataNotEmpty('Table', data);
 }
 
 function testScorersData(code, year) {
-    var scorersData = helper.readJsonFile(helper.stringFormat(config.paths.scorersData, code, year));
+    var data = helper.readJsonFile(helper.stringFormat(config.paths.scorersData, code, year));
 
     it('Scorers should have the right number of players', () => {
-        assert.equal(20, scorersData.length);
+        assert.lengthOf(data, 20);
     });
+
+    testDataNotEmpty('Scorers', data);
 }
 
 function testAssistsData(code, year) {
-    var assistsData = helper.readJsonFile(helper.stringFormat(config.paths.assistsData, code, year));
+    var data = helper.readJsonFile(helper.stringFormat(config.paths.assistsData, code, year));
 
     it('Assists should have the right number of players', () => {
-        assert.equal(20, assistsData.length);
+        assert.lengthOf(data, 20);
+    });
+
+    testDataNotEmpty('Assists', data);
+}
+
+function testDataNotEmpty(dataName, data) {
+    it(dataName + ' should not have empty data', () => {
+        data.forEach((item, index) => {
+            for (var key in item) {
+                assert.notEqual('', item[key], 'Key "' + key + '" is empty for item ' + index);
+            }
+        });
     });
 }
