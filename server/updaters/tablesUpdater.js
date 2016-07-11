25d0a6d6bf454d4b1fd604151d60ed8e147b4f89
softcadbury <romain.parage@gmail.com>
2016-07-11 19:20:23 +0200

    Refacto tables

--- server/updaters/tablesUpdater.js
+++ server/updaters/tablesUpdater.js
@@ -4,13 +4,13 @@ var config = require('../config');
 var helper = require('../helper');
 var leagues = require('../data/leagues');
 
-var tableDataUrl = 'http://www.lequipe.fr/Football/{0}-classement.html';
+var tableDataUrl = 'http://www.worldfootball.net/schedule/{0}-2015-2016-spieltag';
 var leaguesExtended = [
-    { code: leagues.bundesliga.code, url: 'championnat-d-allemagne' },
-    { code: leagues.liga.code, url: 'championnat-d-espagne' },
-    { code: leagues.ligue1.code, url: 'ligue-1' },
-    { code: leagues.serieA.code, url: 'championnat-d-italie' },
-    { code: leagues.premierLeague.code, url: 'championnat-d-angleterre' }
+    { code: leagues.bundesliga.code, url: 'bundesliga' },
+    { code: leagues.liga.code, url: 'esp-primera-division' },
+    { code: leagues.ligue1.code, url: 'fra-ligue-1' },
+    { code: leagues.serieA.code, url: 'ita-serie-a' },
+    { code: leagues.premierLeague.code, url: 'eng-premier-league' }   
 ];
 
 // Updates tables of current year
@@ -26,27 +26,22 @@ function updateData(league) {
         var results = [];
 
         // Gets results
-        $('#col-gauche > section > div.v6-page > table > tbody > tr').each((index, elem) => {
-            if (index < 20) {
+        $('#site > div.white > div.content > div > div:nth-child(7) > div > table.standard_tabelle > tr').each((index, elem) => {
+            if (index > 0 && index <= 20) {
                 results.push({
-                    rank: $(elem).find('td.rang').text(),
-                    team: $(elem).find('td.team .team-label').text(),
-                    points: $(elem).find('td.points').text(),
-                    played: $(elem).find('td:nth-child(6)').text(),
-                    win: $(elem).find('td:nth-child(7)').text(),
-                    draw: $(elem).find('td:nth-child(8)').text(),
-                    lost: $(elem).find('td:nth-child(9)').text(),
-                    goalsFor: $(elem).find('td:nth-child(10)').text(),
-                    goalsAgainst: $(elem).find('td:nth-child(11)').text(),
-                    goalDifference: $(elem).find('td:nth-child(12)').text()
+                    rank: $(elem).find('td:nth-child(1)').text(),
+                    team: $(elem).find('td:nth-child(3) > a').text(),
+                    points: $(elem).find('td:nth-child(10)').text(),
+                    played: $(elem).find('td:nth-child(4)').text(),
+                    win: $(elem).find('td:nth-child(5)').text(),
+                    draw: $(elem).find('td:nth-child(6)').text(),
+                    lost: $(elem).find('td:nth-child(7)').text(),
+                    goalsFor: $(elem).find('td:nth-child(8)').text().split(':')[0],
+                    goalsAgainst: $(elem).find('td:nth-child(8)').text().split(':')[1],
+                    goalDifference: $(elem).find('td:nth-child(9)').text()
                 });
             }
         });
-
-        for (var i = 0; i < results.length; i++) {
-            var path = helper.stringFormat(config.paths.publicImageData, league.code, config.years.current, helper.stringSanitize(results[i].team));
-            results[i].logo = path;
-        }
 
         helper.writeJsonFile(helper.stringFormat(config.paths.tableData, league.code, config.years.current), results);
     });
