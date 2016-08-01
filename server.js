6c05c2e5860619fcbf60dfd984fedd898f229272
Romain Parage <rparage@talentsoft.com>
2016-08-01 15:48:20 +0200

    Remove useless code +cache improvement

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var handlebars = require('express-handlebars');
 var app = express();
 
 // Middlewares configuration
-var oneWeek = 604800000;
-app.use(express.static('build'));
-app.use(express.static('data', { maxAge: oneWeek, extensions: ['png'] }));
+var oneDay = 86400000;
+app.use(express.static('build', { maxAge: oneDay }));
 
 // Handlebars configuration
 app.set('views', 'client/views');
@@ -23,14 +22,6 @@ app.set('view engine', '.hbs');
 // Starts application listening
 app.listen(config.port, (err) => {
     console.log('running on ' + config.port);
-});
-
-// Restore isActive value for every items
-app.use(function (req, res, next) {
-    items.forEach(function (item) {
-        item.isActive = false;
-    });
-    next();
 });
 
 // Regsiters routes
