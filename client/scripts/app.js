4afe1e11b0cc5a7f3933275a20dc9090a2b311d7
Romain Parage <rparage@talentsoft.com>
2016-08-02 15:43:42 +0200

    Code cleaning

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,9 +5,9 @@ var contentElement = document.getElementById('dropdown-content-years');
 if (contentElement) {
     contentElement.style.display = 'none';
 
-    document.addEventListener("click", function (e) {
-        if (e.target.id == 'dropdown-arrow-years' || e.target.id == 'dropdown-label-years') {
-            contentElement.style.display = contentElement.style.display == 'none' ? 'block' : 'none';
+    document.addEventListener('click', function (e) {
+        if (e.target.id === 'dropdown-arrow-years' || e.target.id === 'dropdown-label-years') {
+            contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
         } else {
             contentElement.style.display = 'none';
         }
