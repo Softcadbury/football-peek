13ffafce58f2ab69465d6a71f4743965e78bbe88
Romain Parage <rparage@talentsoft.com>
2016-08-11 19:03:08 +0200

    Complete javascript

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,41 @@
 'use strict';
 
-var yearsElement = document.getElementById('dropdown-content-years');
-if (yearsElement) {
-    yearsElement.style.display = 'none';
-    document.addEventListener('click', function (e) {
-        if (e.target.id === 'dropdown-arrow-years' || e.target.id === 'dropdown-label-years') {
-            yearsElement.style.display = yearsElement.style.display === 'none' ? 'block' : 'none';
-        } else {
-            yearsElement.style.display = 'none';
-        }
-    });
+initDropdown('dropdown-label-years', 'dropdown-arrow-years', 'dropdown-content-years');
+initDropdown('dropdown-label-rounds', 'dropdown-arrow-rounds', 'dropdown-content-rounds');
+
+function initDropdown(labelId, arrowId, contentId) {
+    var contentElement = document.getElementById(contentId);
+    if (contentElement) {
+        contentElement.style.display = 'none';
+        document.addEventListener('click', function (e) {
+            if (e.target.id === arrowId || e.target.id === labelId) {
+                contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
+            } else {
+                contentElement.style.display = 'none';
+            }
+        });
+    }
 }
 
-var roundsElement = document.getElementById('dropdown-content-rounds');
-if (roundsElement) {
-    roundsElement.style.display = 'none';
-    document.addEventListener('click', function (e) {
-        if (e.target.id === 'dropdown-arrow-rounds' || e.target.id === 'dropdown-label-rounds') {
-            roundsElement.style.display = roundsElement.style.display === 'none' ? 'block' : 'none';
+displayRound();
+window.onhashchange = displayRound;
+
+function displayRound() {
+    var roundRequested = location.hash.replace('#/round-', '') || 1;
+    var label = document.getElementById('dropdown-label-rounds');
+    var rounds = document.getElementsByClassName('round-content');
+
+    if (!label) {
+        return;
+    }
+
+    label.innerHTML = 'Round ' + roundRequested;
+
+    for (var i = 0; i < rounds.length; i++) {
+        if (rounds[i].className.indexOf('round-' + roundRequested + '-content') != -1) {
+            rounds[i].style.display = '';
         } else {
-            roundsElement.style.display = 'none';
+            rounds[i].style.display = 'none';
         }
-    });
+    }
 }
