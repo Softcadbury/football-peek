2787ad0b827da7891d0b29efe3afbb25bfcd2d0c
Romain Parage <rparage@talentsoft.com>
2016-08-11 16:54:44 +0200

    Refacto results display

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,25 @@
 'use strict';
 
-var contentElement = document.getElementById('dropdown-content-years');
-
-if (contentElement) {
-    contentElement.style.display = 'none';
-
+var yearsElement = document.getElementById('dropdown-content-years');
+if (yearsElement) {
+    yearsElement.style.display = 'none';
     document.addEventListener('click', function (e) {
         if (e.target.id === 'dropdown-arrow-years' || e.target.id === 'dropdown-label-years') {
-            contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
+            yearsElement.style.display = yearsElement.style.display === 'none' ? 'block' : 'none';
         } else {
-            contentElement.style.display = 'none';
+            yearsElement.style.display = 'none';
+        }
+    });
+}
+
+var roundsElement = document.getElementById('dropdown-content-rounds');
+if (roundsElement) {
+    roundsElement.style.display = 'none';
+    document.addEventListener('click', function (e) {
+        if (e.target.id === 'dropdown-arrow-rounds' || e.target.id === 'dropdown-label-rounds') {
+            roundsElement.style.display = roundsElement.style.display === 'none' ? 'block' : 'none';
+        } else {
+            roundsElement.style.display = 'none';
         }
     });
 }
