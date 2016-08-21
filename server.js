62ad2eebeb857dce4b2a3051a4014e571842e6fc
softcadbury <romain.parage@gmail.com>
2016-08-21 18:37:02 +0200

    Test automatic update of data

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ var handlebars = require('express-handlebars');
 var compression = require('compression');
 var app = express();
 
+// Set leagues current rounds
+leagues.bundesliga.currentRound = helper.getLeagueCurrentRound(leagues.bundesliga.code);
+leagues.liga.currentRound = helper.getLeagueCurrentRound(leagues.liga.code);
+leagues.ligue1.currentRound = helper.getLeagueCurrentRound(leagues.ligue1.code);
+leagues.premierLeague.currentRound = helper.getLeagueCurrentRound(leagues.premierLeague.code);
+leagues.serieA.currentRound = helper.getLeagueCurrentRound(leagues.serieA.code);
+
+// Set cron
+var CronJob = require('cron').CronJob;
+new CronJob('0 */10 * * * *', function () {
+    console.log('run update');
+    require('./server/updaters/tablesUpdater').update(true);
+    require('./server/updaters/resultsUpdater').update(true);
+    require('./server/updaters/scorersUpdater').update(true);
+    require('./server/updaters/assistsUpdater').update(true);
+}, null, true);
+
 // Middlewares configuration
 var tenMinutes = 600000;
 app.use(compression());
@@ -22,13 +39,6 @@ app.engine('.hbs', handlebars({
     defaultLayout: __dirname + '/client/views/_layout.hbs'
 }));
 app.set('view engine', '.hbs');
-
-// Set leagues current rounds
-leagues.bundesliga.currentRound = helper.getLeagueCurrentRound(leagues.bundesliga.code);
-leagues.liga.currentRound = helper.getLeagueCurrentRound(leagues.liga.code);
-leagues.ligue1.currentRound = helper.getLeagueCurrentRound(leagues.ligue1.code);
-leagues.premierLeague.currentRound = helper.getLeagueCurrentRound(leagues.premierLeague.code);
-leagues.serieA.currentRound = helper.getLeagueCurrentRound(leagues.serieA.code);
 
 // Starts application listening
 app.listen(config.port, (err) => {
