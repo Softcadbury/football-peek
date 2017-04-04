04f4b775739f8b7ee96005075e6923b6740f06d0
Romain Parage <rparage@talentsoft.com>
2017-04-04 18:23:06 +0200

    Add the possiblity to switch rounds and groups with the keyboard

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,90 +1,23 @@
 'use strict';
 
-initDropdown('dropdown-label-years', 'dropdown-arrow-years', 'dropdown-content-years');
-initDropdown('dropdown-label-rounds', 'dropdown-arrow-rounds', 'dropdown-content-rounds');
-initDropdown('dropdown-label-groups', 'dropdown-arrow-groups', 'dropdown-content-groups');
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            var leftArrowElement = document.getElementsByClassName('icon-left-open')[0];
 
-manageRoundsDropdown();
-manageGroupsDropdown();
+            if (leftArrowElement) {
+                window.location = leftArrowElement.href;
+            }
+            break;
+        case 39:
+            var rightArrowElement = document.getElementsByClassName('icon-right-open')[0];
 
-window.onhashchange = function () {
-    manageRoundsDropdown();
-    manageGroupsDropdown();
+            if (rightArrowElement) {
+                window.location = rightArrowElement.href;
+            }
+            break;
+        default:
+            break;
+    }
 };
 
-function initDropdown(labelId, arrowId, contentId) {
-    var contentElement = document.getElementById(contentId);
-    if (contentElement) {
-        contentElement.style.display = 'none';
-        document.addEventListener('click', function (e) {
-            if (e.target.id === arrowId || e.target.id === labelId) {
-                contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
-            } else {
-                contentElement.style.display = 'none';
-            }
-        });
-    }
-}
-
-function manageRoundsDropdown() {
-    var roundLabel = document.getElementById('dropdown-label-rounds');
-
-    if (roundLabel) {
-        var roundRequested = location.hash.replace('#/round-', '') || window.currentRound || 1;
-
-        // Set component title
-        roundLabel.innerHTML = 'Round ' + roundRequested;
-
-        // Init previous button
-        var previous = document.getElementById('arrow-rounds-previous');
-        if (roundRequested > 1) {
-            var previousRound = parseInt(roundRequested, 10) - 1;
-            previous.setAttribute('href', '#/round-' + previousRound);
-            previous.style.display = '';
-        } else {
-            previous.style.display = 'none';
-        }
-
-        // Init next button
-        var next = document.getElementById('arrow-rounds-next');
-        if (roundRequested < window.numberOfRounds) {
-            var nextRound = parseInt(roundRequested, 10) + 1;
-            next.setAttribute('href', '#/round-' + nextRound);
-            next.style.display = '';
-        } else {
-            next.style.display = 'none';
-        }
-    }
-}
-
-function manageGroupsDropdown() {
-    var groupLabel = document.getElementById('dropdown-label-groups');
-
-    if (groupLabel) {
-        var groupRequested = location.hash.replace('#/group-', '') || 'a';
-        var groupRequestedIndex = groupRequested.charCodeAt(0);
-
-        // Set component title
-        groupLabel.innerHTML = 'Group ' + groupRequested;
-
-        // Init previous button
-        var previous = document.getElementById('arrow-groups-previous');
-        if (groupRequestedIndex > 96 + 1) {
-            var previousGroup = String.fromCharCode(groupRequestedIndex - 1);
-            previous.setAttribute('href', '#/group-' + previousGroup);
-            previous.style.display = '';
-        } else {
-            previous.style.display = 'none';
-        }
-
-        // Init next button
-        var next = document.getElementById('arrow-groups-next');
-        if (groupRequestedIndex < 96 + window.numberOfGroups) {
-            var nextGroup = String.fromCharCode(groupRequestedIndex + 1);
-            next.setAttribute('href', '#/group-' + nextGroup);
-            next.style.display = '';
-        } else {
-            next.style.display = 'none';
-        }
-    }
-}
