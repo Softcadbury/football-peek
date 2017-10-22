ffd6e87d803fdb07dca2c70ab4e7801241c2e64a
Softcadbury <romain.parage@gmail.com>
2017-10-22 13:17:10 +0200

    Harmonize data of tournaments between final and others

--- server/updaters/tournamentsUpdater.js
+++ server/updaters/tournamentsUpdater.js
@@ -65,10 +65,10 @@ function parseRound(itemExtended, results, roundIndex) {
                     }
 
                     currentMatches.push({
-                        date: $(elem).find('td:nth-child(1)').text(),
+                        date1: $(elem).find('td:nth-child(1)').text(),
                         team1: team1,
                         team2: team2,
-                        score: score,
+                        score1: score,
                         winner: winner
                     });
                 } else {
