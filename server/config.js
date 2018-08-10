224b5821da110e196895cdee81d5acd5a6e0d268
Romain Parage <rparage@talentsoft.com>
2018-08-10 14:26:29 +0200

    Prepare for new season

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@
 function config() {
     var periodFormat = '{0}-{1}';
     var firstHandledYear = 2000;
-    var lastHandledPeriod = 2017;
+    var lastHandledPeriod = 2018;
 
     var currentPeriod = periodFormat.replace('{0}', lastHandledPeriod).replace('{1}', lastHandledPeriod + 1);
     var availablesPeriod = [];
